0fb95a6cc5f37602e9129accc098d9a8374a0145
Connor McCann <cjmccann@bu.edu>
2016-10-18 14:41:23 -0400

    working on live

--- public/javascripts/live_charts.js
+++ public/javascripts/live_charts.js
@@ -142,7 +142,7 @@ $(document).ready(function () {
             { 
                 var num = i + 1;
                 $.get('/get_most_recent_sensor/' + num.toString(), function(data) {
-                    temp = parseFloat(data);
+                    temp = parseFloat(data.reading);
                     console.log('the temp is:');
                     console.log(temp);
                     // console.log('DATA: '+data.date_received);
